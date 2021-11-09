4e7aa3739f831d42be71c71e08dbb4d10a37f6eb
Lauren Hale <lauren.m.hale7@gmail.com>
2021-11-09 15:47:36 -0600

    feat(usermodel)

--- server/db/db.js
+++ server/db/db.js
@@ -1,15 +1,15 @@
 const Sequelize = require('sequelize')
-const pkg = require('../../package.json')
-
-const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
-
+// const pkg = require('../../package.json')
+const dotenv = require('dotenv');
+// const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
+// dotenv.config();
 const config = {
-  logging: false
+  logging: true
 };
 
-if(process.env.LOGGING === 'true'){
-  delete config.logging
-}
+// if(process.env.LOGGING === 'true'){
+//   delete config.logging
+// }
 
 //https://stackoverflow.com/questions/61254851/heroku-postgres-sequelize-no-pg-hba-conf-entry-for-host
 if(process.env.DATABASE_URL){
@@ -21,5 +21,9 @@ if(process.env.DATABASE_URL){
 }
 
 const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`, config)
-module.exports = db
+  // process.env.DATABASE_URL || `postgres://localhost:5432/${process.env.DATABASE_NAME}`, config
+  
+  'postgres://localhost:5432/grace_shopper'
+  )
+
+  module.exports = db
