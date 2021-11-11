4b2aa4012e908621fe125087711d7987d8ab1c6d
Indi Rinearson <indi.rinearson@gmail.com>
2021-11-11 10:31:19 -0800

    React components and routes  outlined

--- client/Routes.js
+++ client/Routes.js
@@ -1,11 +1,23 @@
 import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
 import { withRouter, Route, Switch, Redirect } from "react-router-dom";
-import { Login, Signup } from "./components/AuthForm";
+import { Login, Signup } from "./components/NonPages/AuthForm";
 import Home from "./components/Home";
 import { me } from "./store";
 import AllShells from "./components/AllShells.js";
 import SingleShell from "./components/SingleShell";
+import AdminDash from "./components/Admin/AdminDash";
+import EditShell from "./components/Admin/EditShell";
+import Cart from "./components/Checkout/Cart";
+import CheckOut from "./components/Checkout/CheckOut";
+import OrderConfirmation from "./components/Checkout/OrderConfirmation";
+import About from "./components/About";
+import AccountDetails from "./components/AccountDetails";
+import LogIn from "./components/LogIn";
+import SignUp from "./components/SignUp";
+
+// AccountDetails only viewable if signed in
+// Admin pages only viewable if an admin is mounted
 
 /**
  * COMPONENT
@@ -43,6 +55,19 @@ class Routes extends Component {
           <Route path="/signup" component={Signup} />
           <Route exact path="/shop" component={AllShells} />
           <Route path="/shop/:id" component={SingleShell} />
+          <Route path="/admin/shop/:id/edit" component={EditShell} />
+          <Route exact path="/admin" component={AdminDash} />
+          <Route exact path="/cart" component={Cart} />
+          <Route exact path="/checkout" component={CheckOut} />
+          <Route
+            exact
+            path="/checkout/order-confirmation"
+            component={OrderConfirmation}
+          />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/login" component={LogIn} />
+          <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/account" component={AccountDetails} />
         </Switch>
       </div>
     );
---
+++ client/components/AccountDetails.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const AccountDetails = () => {};
+
+export default AccountDetails;
