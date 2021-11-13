7a3152345dc539f5b80e240efb2dd843ccab48b9
Emma Strickland <emlstrick@gmail.com>
2021-11-13 13:59:36 -0500

    Fix order prototype method for adding to cart

--- server/db/models/Order.js
+++ server/db/models/Order.js
@@ -38,13 +38,23 @@ Order_Details.belongsTo(Shell)
 
 // console.log(Order.prototype)
 
+// update order_details based on shell first then update order based on order_details
 Order.prototype.addToCart = async function (shell) {
-  await this.addShell(shell.id)
-  this.numberOfItems++;
-  this.subTotal += shell.price;
+  // creating line in order_details with shellId and orderId 
+  // check if this shell already exists
+  console.log('order beofre adding shell: ', this)
+  const shellDetails = await this.addShell(shell.id)
+
+  // console.log('shell arg', shell)
+  // console.log('added shell: ', shellDetails)
+  await this.update(
+    {
+      numberOfItems: this.numberOfItems + 1,
+      subTotal: this.subTotal += shell.price
+    }
+  )
+  console.log('order after adding shell: ', this)
   return this;
 }
-
-// console.log("from models/Order.js", Order.prototype)
 
 module.exports = Order;
