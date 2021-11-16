a0c89fdbfc9ccb5e547071e004e1ca2952c204c1
Liat Guvenc <liatg812@gmail.com>
2021-11-16 17:53:27 -0500

    worked on admin slice

--- client/components/Admin/ShellsTable.js
+++ client/components/Admin/ShellsTable.js
@@ -1,7 +1,60 @@
+
 import React from "react";
+import { connect } from "react-redux";
+import {
+  fetchAllShells,
+  updateShell,
+  deleteShell,
+  createNewShell,
+} from "../../store/allProducts";
 
-// follow format of UsersTable. List the URL for the image and not the picture itself.
+// const SingleUser = (props) => {
+//   const user = props.user;
+//   return (
+//     <tr>
+//       <td>{user.id}</td>
+//       <td>{user.email}</td>
+//       <td>{user.password}</td>
+//       <td>{`${user.isAdmin}`}</td>
+//     </tr>
+//   );
+// };
 
-const ShellsTable = () => {};
+// class ShellsTable extends React.Component {
+//   componentDidMount() {
+//     this.props.getAll();
+//   }
 
-export default ShellsTable;
+//   render() {
+//     let users = this.props.users || [];
+//     return (
+//       <table>
+//         <tr>
+//           <th>Id</th>
+//           <th>Email</th>
+//           <th>Password</th>
+//           <th>Admin</th>
+//         </tr>
+//         {users.map((user) => (
+//           <SingleUser key={user.id} user={user} />
+//         ))}
+//       </table>
+//     );
+//   }
+// }
+
+// const mapState = (state) => {
+//   return {
+//     users: state.users
+//   };
+// };
+
+// const mapDispatch = (dispatch) => {
+//   return {
+//     getAll: () => dispatch(getAllUsers()),
+//     update: (user) => dispatch(updateUser(user)),
+//     delete: (id) => dispatch(deleteUser(user)),
+//   };
+// };
+
+// export default connect(mapState, mapDispatch)(ShellsTable); // change to connected componenet
