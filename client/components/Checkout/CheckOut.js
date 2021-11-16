b135262f2464899a312361a0bf9b7792cc683b85
Lauren Hale <lauren.m.hale7@gmail.com>
2021-11-15 21:22:20 -0600

    simulated a checkout

--- client/components/Checkout/CheckOut.js
+++ client/components/Checkout/CheckOut.js
@@ -1,5 +1,24 @@
 import React from "react";
+import { connect } from "react-redux";
+import history from "../../history";
+const CheckOut = (props) => {
+    
 
-const CheckOut = () => {};
+    function confirmOrder(){
+        console.log('confirmed')
+        history.push("/checkout/order-confirmation")
+    }
+  return (
+    <div>
+      <h1>CheckOut</h1>
+      <button onClick={confirmOrder}>Submit Order</button>
+    </div>
+  );
+};
 
-export default CheckOut;
+const mapState = (state) => {
+  return {
+    userId: state.auth.id,
+  };
+};
+export default connect(mapState)(CheckOut);
