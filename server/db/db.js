056bda9673b66e9cf5531eac0148a9db642d096a
Indi Rinearson <indi.rinearson@gmail.com>
2021-11-16 12:21:58 -0800

    first deploy

--- server/db/db.js
+++ server/db/db.js
@@ -1,10 +1,10 @@
 const Sequelize = require("sequelize");
-const pkg = require('../../package.json')
-const dotenv = require('dotenv').config();
+const pkg = require("../../package.json");
+const dotenv = require("dotenv").config();
 // const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 // dotenv.config();
 const config = {
-  logging: false
+  logging: false,
 };
 
 if (process.env.LOGGING === "true") {
@@ -20,12 +20,11 @@ if (process.env.DATABASE_URL) {
   };
 }
 
-const dbName = process.env.NODE_ENV === 'test' ? `${pkg.name}_test` : pkg.name;
+const dbName = process.env.NODE_ENV === "test" ? `${pkg.name}_test` : pkg.name;
 
 const db = new Sequelize(
-  // process.env.DATABASE_URL || `postgres://localhost:5432/${process.env.DATABASE_NAME}`, config
-
-  `postgres://localhost:5432/${dbName}`, config
-)
+  process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`,
+  config
+);
 
 module.exports = db;
