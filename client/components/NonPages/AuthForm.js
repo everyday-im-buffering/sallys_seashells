29c93e22a28578959f6bee49943a3ab7589e0888
Emma Strickland <emlstrick@gmail.com>
2021-11-17 12:38:07 -0500

    Install formik and begin validation

--- client/components/NonPages/AuthForm.js
+++ client/components/NonPages/AuthForm.js
@@ -1,27 +1,77 @@
 import React from "react";
 import { connect } from "react-redux";
 import { authenticate } from "../../store";
+import * as Yup from "yup";
+import { useFormik, Formik, Form, Field, ErrorMessage } from 'formik';
+
+// const SignupSchema = Yup.object().shape({
+//   email: Yup.string()
+//     .email("Invalid email addresss")
+//     .required("Required"),
+//   password: Yup.string()
+//     .min(8, "Must be greater than 8 characters")
+//     .required("Required"),
+// });
+
+const validate = values => {
+  const errors = {};
+
+  if (!values.email) {
+    errors.email = 'Required';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email address';
+  }
+  if (!values.password) {
+    errors.password = 'Required';
+  } else if (values.password.length <= 8) {
+    errors.password = 'Must be at least 8 characters';
+  }
+
+  return errors;
+};
 
 /**
  * COMPONENT
  */
 const AuthForm = props => {
   const { name, displayName, handleSubmit, error } = props
-
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validate,
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
   return (
     <div>
-      <form onSubmit={handleSubmit} name={name}>
+      <h1>{displayName}</h1>
+      <form onSubmit={formik.handleSubmit} name={name}>
         <div>
           <label htmlFor="email">
             <small>Email</small>
           </label>
-          <input name="email" type="text" />
+          <input
+            name="email"
+            type="text"
+            onChange={formik.handleChange}
+            value={formik.values.email}
+          />
+          {formik.errors.email ? <div>{formik.errors.email}</div> : null}
         </div>
         <div>
           <label htmlFor="password">
             <small>Password</small>
           </label>
-          <input name="password" type="password" />
+          <input
+            name="password"
+            type="password"
+            onChange={formik.handleChange}
+            value={formik.values.password}
+          />
+          {formik.errors.password ? <div>{formik.errors.password}</div> : null}
         </div>
         <div>
           <button type="submit">{displayName}</button>
