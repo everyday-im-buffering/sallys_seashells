44081c50f6ab41312aea88e8c6f83cbaed9a51d0
Emma Strickland <emlstrick@gmail.com>
2021-11-17 15:24:41 -0500

    Incorporate more Formik stuff

--- client/components/NonPages/AuthForm.js
+++ client/components/NonPages/AuthForm.js
@@ -2,83 +2,47 @@ import React from "react";
 import { connect } from "react-redux";
 import { authenticate } from "../../store";
 import * as Yup from "yup";
-import { useFormik, Formik, Form, Field, ErrorMessage } from 'formik';
-
-// const SignupSchema = Yup.object().shape({
-//   email: Yup.string()
-//     .email("Invalid email addresss")
-//     .required("Required"),
-//   password: Yup.string()
-//     .min(8, "Must be greater than 8 characters")
-//     .required("Required"),
-// });
-
-const validate = values => {
-  const errors = {};
-
-  if (!values.email) {
-    errors.email = 'Required';
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = 'Invalid email address';
-  }
-  if (!values.password) {
-    errors.password = 'Required';
-  } else if (values.password.length <= 8) {
-    errors.password = 'Must be at least 8 characters';
-  }
-
-  return errors;
-};
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 /**
  * COMPONENT
  */
-const AuthForm = props => {
+const AuthForm = (props) => {
+  // const [clearForm, setClearForm] = useState('');
   const { name, displayName, handleSubmit, error } = props
-  const formik = useFormik({
-    initialValues: {
-      email: '',
-      password: '',
-    },
-    validate,
-    onSubmit: values => {
-      alert(JSON.stringify(values, null, 2));
-    },
-  });
   return (
-    <div>
-      <h1>{displayName}</h1>
-      <form onSubmit={formik.handleSubmit} name={name}>
-        <div>
-          <label htmlFor="email">
-            <small>Email</small>
-          </label>
-          <input
-            name="email"
-            type="text"
-            onChange={formik.handleChange}
-            value={formik.values.email}
-          />
-          {formik.errors.email ? <div>{formik.errors.email}</div> : null}
-        </div>
-        <div>
-          <label htmlFor="password">
-            <small>Password</small>
-          </label>
-          <input
-            name="password"
-            type="password"
-            onChange={formik.handleChange}
-            value={formik.values.password}
-          />
-          {formik.errors.password ? <div>{formik.errors.password}</div> : null}
-        </div>
-        <div>
-          <button type="submit">{displayName}</button>
-        </div>
-        {error && error.response && <div> {error.response.data} </div>}
-      </form>
-    </div>
+    <Formik
+      // const formik = useFormik({
+      initialValues={
+        {
+          email: '',
+          password: ''
+        }}
+      validationSchema={Yup.object({
+        email: Yup.string().email('Invalid email address').required('Required'),
+        password: Yup.string()
+          .min(8, 'Must be at least 8 characters')
+          .required('Required'),
+      })}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      <Form>
+        <label htmlFor="email">Email</label>
+        <Field name="email" type="text" />
+        <ErrorMessage name="email" />
+
+        <label htmlFor="password">Password</label>
+        <Field name="password" type="password" />
+        <ErrorMessage name="password" />
+
+        <button type="submit">{displayName}</button>
+      </Form>
+    </Formik>
   );
 };
 
@@ -112,7 +76,7 @@ const mapDispatch = (dispatch) => {
       const formName = evt.target.name
       const email = evt.target.email.value
       const password = evt.target.password.value
-      dispatch(authenticate(email, password, formName))
+      dispatch(authenticate(email, password, formName));
     }
   }
 }
