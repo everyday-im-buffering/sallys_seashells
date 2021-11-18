1392bc20b5588b97d8ddccb3358d9edb13ce7849
Emma Strickland <emlstrick@gmail.com>
2021-11-18 10:34:38 -0500

    Final

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const app = express();
 module.exports = app;
 const dotenv = require("dotenv").config();
 
-app.use(cookieParser("secret"));
+app.use(cookieParser(process.env.COOKIE_SECRET));
 
 app.use(morgan("dev"));
 
